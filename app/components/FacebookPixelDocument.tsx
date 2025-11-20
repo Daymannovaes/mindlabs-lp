76e5da4d9b4ec7603c270a0e6a00db5c018cf260
Dayman Novaes <daymannovaes@gmail.com>
2025-11-20 13:07:31 -0300

    add fb pixel

---
+++ app/components/FacebookPixelDocument.tsx
@@ -0,0 +1,42 @@
+const FB_PIXEL_ID = "25168737529434799";
+
+// This component is specifically designed for _document.tsx
+export function FacebookPixelHead() {
+  return (
+    <>
+      {/* Facebook Pixel Script */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '25168737529434799');
+
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+      {/* <meta name="facebook-domain-verification" content="lb3mkgfapxrjap6h1faiytcyrlghdm" /> */}
+    </>
+  );
+}
+
+export function FacebookPixelNoscript() {
+  return (
+    <>
+      {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
+      <img
+        height="1"
+        width="1"
+        style={{display: 'none'}}
+        src={`https://www.facebook.com/tr?id=25168737529434799&ev=PageView&noscript=1`}
+      />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+
+import { FacebookPixelHead, FacebookPixelNoscript } from "./components/FacebookPixelDocument";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <FacebookPixelHead />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <FacebookPixelNoscript />
+        </noscript>
         {children}
       </body>
     </html>
